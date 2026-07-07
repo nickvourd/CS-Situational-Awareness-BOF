7beb41e6427226e3daeccf2d12dc61ae3a2d110d
nickvourd <32869078+nickvourd@users.noreply.github.com>
2026-07-07 13:01:13 +0300

    Update adrblst output

--- src/SA/adrblist/entry.c
+++ src/SA/adrblist/entry.c
@@ -7,8 +7,17 @@ typedef struct {
 	char sam[256];
 	char type[64];
 	char whenDeleted[64];
+	char whenChanged[64];
 	char dn[512];
 	char lastKnownParent[512];
+	char objectSid[256];
+	char description[512];
+	char servicePrincipalNames[1024];
+	char delegateTo[1024];
+	char members[2048];
+	int adminCount;
+	int userAccountControl;
+	BOOL hasManagedPassword;
 } DELETED_OBJECT;
 
 BOOL GetRootDN(LDAP* ld, char** rootDN) {
@@ -117,12 +126,103 @@ void GetStringValue(LDAP* ld, LDAPMessage* entry, const char* attr, char* value_
 	}
 }
 
+// Get integer value from entry
+int GetIntValue(LDAP* ld, LDAPMessage* entry, const char* attr) {
+	PCHAR* values = WLDAP32$ldap_get_values(ld, entry, (PSTR)attr);
+	if (values && values[0]) {
+		int val = 0;
+		for (char* p = values[0]; *p; p++) {
+			if (*p >= '0' && *p <= '9') {
+				val = val * 10 + (*p - '0');
+			} else {
+				break;
+			}
+		}
+		WLDAP32$ldap_value_free(values);
+		return val;
+	}
+	return 0;
+}
+
+// Convert binary SID to string format (S-1-5-21-...)
+void ConvertSIDToString(unsigned char* sid, int len, char* sid_str, int sid_str_len) {
+	MSVCRT$memset(sid_str, 0, sid_str_len);
+	if (len < 8) {
+		MSVCRT$strcpy(sid_str, "invalid");
+		return;
+	}
+
+	unsigned char revision = sid[0];
+	unsigned char sub_auth_count = sid[1];
+	unsigned long long authority = 0;
+
+	// Read authority (6 bytes, big-endian)
+	for (int i = 0; i < 6; i++) {
+		authority = (authority << 8) | sid[2 + i];
+	}
+
+	int offset = 0;
+	offset += MSVCRT$sprintf(sid_str + offset, "S-%u-%llu", revision, authority);
+
+	// Read sub-authorities (4 bytes each, little-endian)
+	int sub_auth_offset = 8;
+	for (int i = 0; i < sub_auth_count && sub_auth_offset + 4 <= len; i++) {
+		unsigned long sub_auth = sid[sub_auth_offset] |
+								(sid[sub_auth_offset + 1] << 8) |
+								(sid[sub_auth_offset + 2] << 16) |
+								(sid[sub_auth_offset + 3] << 24);
+		offset += MSVCRT$sprintf(sid_str + offset, "-%lu", sub_auth);
+		sub_auth_offset += 4;
+	}
+}
+
+// Get binary values and concatenate as strings (for multivalued attrs)
+void GetStringValues(LDAP* ld, LDAPMessage* entry, const char* attr, char* values_out, int values_len) {
+	MSVCRT$memset(values_out, 0, values_len);
+	PCHAR* values = WLDAP32$ldap_get_values(ld, entry, (PSTR)attr);
+	if (values) {
+		int offset = 0;
+		for (int i = 0; values[i] != NULL && offset < values_len - 1; i++) {
+			if (i > 0 && offset < values_len - 2) {
+				values_out[offset++] = ';';
+				values_out[offset++] = ' ';
+			}
+			int len = MSVCRT$strlen(values[i]);
+			if (offset + len > values_len - 1) {
+				len = values_len - 1 - offset;
+			}
+			MSVCRT$memcpy(values_out + offset, values[i], len);
+			offset += len;
+		}
+		values_out[offset] = '\0';
+		WLDAP32$ldap_value_free(values);
+	}
+}
+
+// Get binary attribute values
+struct berval** GetBinaryValues(LDAP* ld, LDAPMessage* entry, const char* attr) {
+	return WLDAP32$ldap_get_values_lenA(ld, entry, (PSTR)attr);
+}
+
+// Check if binary attribute exists and is not empty
+BOOL HasBinaryAttribute(LDAP* ld, LDAPMessage* entry, const char* attr) {
+	struct berval** values = GetBinaryValues(ld, entry, attr);
+	if (values && values[0] && values[0]->bv_len > 0) {
+		WLDAP32$ldap_value_free_len(values);
+		return TRUE;
+	}
+	return FALSE;
+}
+
 ULONG ListDeletedObjects(LDAP* ld, char* rootDN) {
 	LDAPMessage* res = NULL;
 	char searchDN[512] = {0};
 	char* attrs[] = {
-		"sAMAccountName", "objectClass", "whenDeleted", "lastKnownParent",
-		"userAccountControl", "adminCount", "description", NULL
+		"sAMAccountName", "objectClass", "objectSid", "sIDHistory",
+		"whenDeleted", "whenChanged", "lastKnownParent",
+		"userAccountControl", "adminCount", "description",
+		"servicePrincipalName", "msDS-AllowedToDelegateTo", "member",
+		"msDS-ManagedPasswordId", NULL
 	};
 	ULONG ldapErr = 0;
 	LDAP_TIMEVAL timeout = {0};
@@ -170,40 +270,119 @@ ULONG ListDeletedObjects(LDAP* ld, char* rootDN) {
 
 	entryCount = WLDAP32$ldap_count_entries(ld, res);
 	internal_printf("[+] Found %lu deleted object(s)\n\n", entryCount);
-	internal_printf("%-30s | %-20s | %-15s | %s\n", "Name", "SAMAccountName", "Type", "Deleted");
-	internal_printf("%.*s\n", 100, "====================================================================================================");
+
+	// Allocate large buffers once before loop to avoid stack overflow
+	DELETED_OBJECT* obj = (DELETED_OBJECT*)intAlloc(sizeof(DELETED_OBJECT));
+	if (!obj) {
+		internal_printf("[-] Memory allocation failed\n");
+		WLDAP32$ldap_msgfree(res);
+		return 0;
+	}
 
 	LDAPMessage* entry = WLDAP32$ldap_first_entry(ld, res);
 	ULONG displayed = 0;
 
 	while (entry) {
-		char cn[256] = {0};
-		char sam[256] = {0};
-		char type[64] = {0};
-		char whenDeleted[64] = {0};
-		char* dn = NULL;
+		MSVCRT$memset(obj, 0, sizeof(DELETED_OBJECT));
+		char* dnPtr = NULL;
+		int i = 0;
 
-		dn = WLDAP32$ldap_get_dn(ld, entry);
-		if (dn) {
-			ParseDeletedDN(dn, cn, sizeof(cn));
-			WLDAP32$ldap_memfree(dn);
+		dnPtr = WLDAP32$ldap_get_dn(ld, entry);
+		if (dnPtr) {
+			ParseDeletedDN(dnPtr, obj->cn, sizeof(obj->cn));
+			MSVCRT$strcpy(obj->dn, dnPtr);
+			WLDAP32$ldap_memfree(dnPtr);
 		}
 
-		GetStringValue(ld, entry, "sAMAccountName", sam, sizeof(sam));
-		ExtractObjectType(ld, entry, type, sizeof(type));
-		GetStringValue(ld, entry, "whenDeleted", whenDeleted, sizeof(whenDeleted));
+		GetStringValue(ld, entry, "sAMAccountName", obj->sam, sizeof(obj->sam));
+		ExtractObjectType(ld, entry, obj->type, sizeof(obj->type));
+		GetStringValue(ld, entry, "whenDeleted", obj->whenDeleted, sizeof(obj->whenDeleted));
+		GetStringValue(ld, entry, "whenChanged", obj->whenChanged, sizeof(obj->whenChanged));
+		GetStringValue(ld, entry, "lastKnownParent", obj->lastKnownParent, sizeof(obj->lastKnownParent));
+		GetStringValue(ld, entry, "description", obj->description, sizeof(obj->description));
+		GetStringValues(ld, entry, "servicePrincipalName", obj->servicePrincipalNames, sizeof(obj->servicePrincipalNames));
+		GetStringValues(ld, entry, "msDS-AllowedToDelegateTo", obj->delegateTo, sizeof(obj->delegateTo));
+		GetStringValues(ld, entry, "member", obj->members, sizeof(obj->members));
+		obj->adminCount = GetIntValue(ld, entry, "adminCount");
+		obj->userAccountControl = GetIntValue(ld, entry, "userAccountControl");
+		obj->hasManagedPassword = HasBinaryAttribute(ld, entry, "msDS-ManagedPasswordId");
 
-		// Truncate long strings for display
-		if (MSVCRT$strlen(cn) > 30) cn[27] = cn[28] = cn[29] = '.';
-		if (MSVCRT$strlen(sam) > 20) sam[17] = sam[18] = sam[19] = '.';
+		// Extract objectSid
+		struct berval** sidValues = GetBinaryValues(ld, entry, "objectSid");
+		if (sidValues && sidValues[0]) {
+			ConvertSIDToString((unsigned char*)sidValues[0]->bv_val, sidValues[0]->bv_len, obj->objectSid, sizeof(obj->objectSid));
+			WLDAP32$ldap_value_free_len(sidValues);
+		}
 
-		internal_printf("%-30s | %-20s | %-15s | %s\n", cn, sam, type, whenDeleted);
+		// Display object details
+		internal_printf("\n[*] %s (%s)\n", obj->cn, obj->type);
+		internal_printf("    DN                      : %s\n", obj->dn);
+
+		if (obj->sam[0]) {
+			internal_printf("    SAM                     : %s\n", obj->sam);
+		}
+
+		if (obj->objectSid[0]) {
+			internal_printf("    ObjectSID               : %s\n", obj->objectSid);
+		}
+
+		// Check for security-relevant sIDHistory
+		struct berval** sidHistoryValues = GetBinaryValues(ld, entry, "sIDHistory");
+		if (sidHistoryValues) {
+			for (i = 0; sidHistoryValues[i] != NULL; i++) {
+				char sidHistStr[256] = {0};
+				ConvertSIDToString((unsigned char*)sidHistoryValues[i]->bv_val, sidHistoryValues[i]->bv_len, sidHistStr, sizeof(sidHistStr));
+				internal_printf("    sIDHistory              : %s\n", sidHistStr);
+			}
+			WLDAP32$ldap_value_free_len(sidHistoryValues);
+		}
+
+		if (obj->adminCount > 0) {
+			internal_printf("    adminCount              : %d *** HIGH VALUE ***\n", obj->adminCount);
+		}
+
+		if (obj->servicePrincipalNames[0]) {
+			internal_printf("    SPNs (Kerberoastable)   : %s\n", obj->servicePrincipalNames);
+		}
+
+		if (obj->delegateTo[0]) {
+			internal_printf("    Constrained Delegation  : %s *** DELEGATION ***\n", obj->delegateTo);
+		}
+
+		if (obj->userAccountControl & 0x1000000) {
+			internal_printf("    UAC Flag                : TRUSTED_TO_AUTH *** DELEGATION ***\n");
+		}
+
+		if (obj->description[0]) {
+			internal_printf("    Description             : %s\n", obj->description);
+		}
+
+		if (obj->members[0]) {
+			internal_printf("    Group Members           : %s\n", obj->members);
+		}
+
+		if (obj->hasManagedPassword) {
+			internal_printf("    gMSA Password           : PRESENT *** OFFLINE RECOVERY ***\n");
+		}
+
+		if (obj->lastKnownParent[0]) {
+			internal_printf("    Last Known Parent       : %s\n", obj->lastKnownParent);
+		}
+
+		if (obj->whenDeleted[0]) {
+			internal_printf("    Deleted                 : %s\n", obj->whenDeleted);
+		}
+
+		if (obj->whenChanged[0]) {
+			internal_printf("    Last Changed            : %s\n", obj->whenChanged);
+		}
 
 		displayed++;
 		entry = WLDAP32$ldap_next_entry(ld, entry);
 	}
 
-	internal_printf("%.*s\n", 100, "====================================================================================================");
+	intFree(obj);
+
 	internal_printf("\n[+] Displayed %lu objects\n\n", displayed);
 
 	WLDAP32$ldap_msgfree(res);
